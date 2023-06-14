63849c4d0bb0bead02cea09df17ba80c113ee5ec
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-14 21:00:45 +0200

    added Features page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const About = () => {
-  return <div id="about">About</div>;
+  return (
+    <div id="about" className="my-[5rem]">
+      <h1 className="text-center md:text-5xl xs:text-3xl my-6 font-rufina text-textColor">
+        Who we are
+      </h1>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,7 +1,117 @@
 import React from "react";
 
+// react-carousel
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import {
+  gallery_1,
+  gallery_2,
+  gallery_3,
+  gallery_4,
+  gallery_5,
+  gallery_6,
+} from "../../assets";
+
 const Projects = () => {
-  return <div id="projects">Projects</div>;
+  const ourProjects = [
+    {
+      image: gallery_1,
+      title: "Cherish",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorum illum vitae fuga nulla maiores suscipit rerum, autem tenetur sed a laboriosam nostrum fugiat quam laborum? Repellat officia pariatur illo!",
+    },
+    {
+      image: gallery_2,
+      title: "Rebuild",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorum illum vitae fuga nulla maiores suscipit rerum, autem tenetur sed a laboriosam nostrum fugiat quam laborum? Repellat officia pariatur illo!",
+    },
+    {
+      image: gallery_3,
+      title: "Observe",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorum illum vitae fuga nulla maiores suscipit rerum, autem tenetur sed a laboriosam nostrum fugiat quam laborum? Repellat officia pariatur illo!",
+    },
+    {
+      image: gallery_4,
+      title: "Grow",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorum illum vitae fuga nulla maiores suscipit rerum, autem tenetur sed a laboriosam nostrum fugiat quam laborum? Repellat officia pariatur illo!",
+    },
+    {
+      image: gallery_5,
+      title: "Research",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorum illum vitae fuga nulla maiores suscipit rerum, autem tenetur sed a laboriosam nostrum fugiat quam laborum? Repellat officia pariatur illo!",
+    },
+    {
+      image: gallery_6,
+      title: "Test",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorum illum vitae fuga nulla maiores suscipit rerum, autem tenetur sed a laboriosam nostrum fugiat quam laborum? Repellat officia pariatur illo!",
+    },
+  ];
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 600 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 600, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <div id="projects" className="my-[5rem]">
+      <h1 className="text-center md:text-5xl xs:text-3xl my-6 font-rufina text-textColor">
+        Our projects
+      </h1>
+
+      <div>
+        <Carousel
+          swipeable={true}
+          draggable={false}
+          infinite={true}
+          keyBoardControl={true}
+          customTransition="all .8"
+          transitionDuration={500}
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+          className="carousel"
+          responsive={responsive}
+        >
+          {ourProjects.map((project) => (
+            <div key={project.length} className="w-full p-4 pl-[2.3rem]">
+              <img
+                src={project.image}
+                alt="projectImages"
+                className="w-[400px] h-[400px] rounded-xl"
+              />
+              <h1 className="text-center my-5 font-rufina font-bold text-2xl text-textColor">
+                {project.title}
+              </h1>
+              <p className="text-center font-oxygen text-lg text-textColor pr-2">
+                {project.description}
+              </p>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
