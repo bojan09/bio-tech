913611ef183a4b0d7fe9a139610041fc52916d1c
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-14 20:27:10 +0200

    added Features page

--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 const Features = () => {
-  return <div id="features">Features</div>;
+  return (
+    <div
+      id="features"
+      className="my-[5rem] flex flex-col justify-center items-center gap-10"
+    >
+      <h1 className="text-center md:text-5xl xs:text-3xl my-6 font-rufina text-textColor">
+        Reliable aids you recive from us
+      </h1>
+
+      <div className="grid md:flex flex-wrap justify-center xs:gap-10 md:gap-2 md:w-[40%]">
+        <div className="polygon bg-primaryBackground w-[13rem] h-[14rem] flex items-center bg-gradient-to-tr from-primaryBackground to-primaryButton">
+          <p className="text-whiteColor font-oxygen text-lg p-2 text-center cursor-pointer">
+            Freshwater and sediment sampling and analysis
+          </p>
+        </div>
+
+        <div className="polygon bg-primaryBackground w-[13rem] h-[14rem] flex items-center bg-gradient-to-tr from-primaryBackground to-primaryButton">
+          <p className="text-whiteColor font-oxygen text-lg p-2 text-center cursor-pointer">
+            eDNA sampling and analysis
+          </p>
+        </div>
+
+        <div className="polygon bg-primaryBackground w-[13rem] h-[14rem] flex items-center bg-gradient-to-tr from-primaryBackground to-primaryButton">
+          <p className="text-whiteColor font-oxygen text-lg p-2 text-center cursor-pointer">
+            Traditional knowledge interviews and collation
+          </p>
+        </div>
+
+        <div className="md:mt-[-4rem] polygon bg-primaryBackground w-[13rem] h-[14rem] flex items-center bg-gradient-to-tr from-primaryBackground to-primaryButton">
+          <p className="text-whiteColor font-oxygen text-lg p-2 px-[1.55rem] text-center cursor-pointer">
+            Document reviewer
+          </p>
+        </div>
+
+        <div className="md:mt-[-4rem] polygon bg-primaryBackground w-[13rem] h-[14rem] flex items-center bg-gradient-to-tr from-primaryBackground to-primaryButton">
+          <p className="text-whiteColor font-oxygen text-lg p-2 text-center cursor-pointer">
+            Community outreach and Consultation
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
