90802586b30450f8ab1771fa9c473d9a89200087
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-14 17:17:08 +0200

    added Navbar component, setup custom colors & fonts in Tailwind

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,61 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="p-6 justify-evenly flex items-center sticky bg-whiteColor">
+      <div className="mr-[7rem] logo bg-gradient-to-r from-primaryBackground to-primaryButton">
+        <a
+          href="#"
+          className="font-rufina font-semibold text-gray-50 text-[20px]"
+        >
+          BioTech
+        </a>
+      </div>
+
+      <ul className="flex gap-4">
+        <li>
+          <a
+            className="text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#"
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#features"
+          >
+            Features
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#projects"
+          >
+            Projects
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#about"
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#contact"
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         rufina: ["Rufina", "serif"],
         oxygen: ["Oxygen", "sans-serif"],
       },
+      colors: {
+        textColor: "#020402",
+        whiteColor: "#f9fcf8",
+        primaryButton: "#8ecc96",
+        secondaryButton: "#edf7f3",
+        primaryBackground: "#4ca985",
+      },
     },
   },
   plugins: [],
