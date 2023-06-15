6d2e251708568930abd24f7f5fec36e87aac6e6e
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-15 09:34:43 +0200

    added Footer page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <ul className="flex justify-center items-center md:gap-14 bg-whiteColor py-10 mt-[4rem] flex-wrap">
+        <li className="mb-2">
+          <a
+            className="text-[16px] md:text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#home"
+          >
+            Home
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            className="text-[16px] md:text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#features"
+          >
+            Features
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            className="text-[16px] md:text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#projects"
+          >
+            Projects
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            className="text-[16px] md:text-[20px] font-oxygen uppercase font-medium focus:bg-gradient-to-r from-primaryBackground to-primaryButton focus-within:text-white p-2"
+            href="#about"
+          >
+            About
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
